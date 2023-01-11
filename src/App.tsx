a247c511559b6b1644edcfac08be5e7a952cbca1
ronnahom96 <ronnahom96@gmail.com>
2023-01-11 22:09:09 +0200

    add words

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./App.css";
 import { useTimer } from "./hooks/useTimer";
+import words from "./assets/words.json";
 
 const App: React.FC = () => {
   const timer = useTimer(60);
 
   return (
-    <>
-      <h1 className='title'>Type Racer</h1>;
+    <div className='container'>
+      <h1 className='title'>Type Racer</h1>
       <h2 className='timer'>Timer: {timer}</h2>
-    </>
+      {words.map((word, index) => (
+        <h3 key={index}>{word}</h3>
+      ))}
+    </div>
   );
 };
 
