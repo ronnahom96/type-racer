9457f73dff03953f266d401f8108713749605033
ronnahom96 <ronnahom96@gmail.com>
2023-01-12 12:51:25 +0200

    update app to use game over

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import "./App.css";
 import words from "./assets/words.json";
+import GameOver from "./components/gameOver/gameOver";
 import WordList from "./components/wordList/wordList";
 import { useTimer } from "./hooks/useTimer";
 
 const App: React.FC = () => {
-  const [timer, isTimerActive, setIsTimerActive] = useTimer(60);
+  const [timer, isTimerActive, startOverTimer] = useTimer(2);
 
   return (
     <div className='container'>
       <h1 className='title'>Type Racer</h1>
       <h2 className='timer'>Timer: {timer}</h2>
-      {isTimerActive ? <WordList wordList={words} /> : <h1>Game over</h1>}
-      <input placeholder='Type here...' className='input' type='text'></input>
+      {isTimerActive ? (
+        <>
+          <WordList wordList={words} />
+          <input placeholder='Type here...' className='input' type='text'></input>
+        </>) : (
+        <GameOver handlePlayAgain={() => startOverTimer()} />
+      )}
     </div>
   );
 };
