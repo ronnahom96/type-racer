8a1ada7f89d306d8f34cf537f4755afb34e59bd8
ronnahom96 <ronnahom96@gmail.com>
2023-01-12 12:50:58 +0200

    add statistics

---
+++ src/components/statistics/statistics.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./statistics.css";
+
+interface StatisticsProps {
+}
+
+const Statistics: React.FC<StatisticsProps> = () => {
+  return (
+    <div className="statistics-container">
+      <h2>statistics here</h2>
+    </div>
+  );
+};
+
+export default Statistics;
