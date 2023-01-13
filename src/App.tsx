72d85f4e15d4d68fd75084fd8b6fc69d7edb5040
ronnahom96 <ronnahom96@gmail.com>
2023-01-13 14:33:41 +0200

    remove css from app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
-import React from "react";
-import "./App.css";
-import GameHandler from "./pages/GameHandler";
+import React from 'react';
+import GameHandler from './pages/GameHandler';
 
 const App: React.FC = () => {
   return <GameHandler />;
