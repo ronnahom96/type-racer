9457902af03d334fd2af8c6db411daae271def0c
ronnahom96 <ronnahom96@gmail.com>
2023-01-13 14:16:18 +0200

    move logic from app to game handler page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import React from "react";
 import "./App.css";
-import words from "./assets/words.json";
-import GameOver from "./components/gameOver/GameOver";
-import WordList from "./components/wordList/WordList";
-import { useTimer } from "./hooks/useTimer";
+import GameHandler from "./pages/GameHandler";
 
 const App: React.FC = () => {
-  const [timer, isTimerActive, startOverTimer] = useTimer(2);
-
-  return (
-    <div className='container'>
-      <h1 className='title'>Type Racer</h1>
-      <h2 className='timer'>Timer: {timer}</h2>
-      {isTimerActive ? (
-        <>
-          <WordList wordList={words} />
-          <input placeholder='Type here...' className='input' type='text'></input>
-        </>) : (
-        <GameOver handlePlayAgain={() => startOverTimer()} />
-      )}
-    </div>
-  );
+  return <GameHandler />;
 };
 
 export default App;
