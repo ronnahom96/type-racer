fb8b533fb51dc34513baa73091a73117585b06f3
ronnahom96 <ronnahom96@gmail.com>
2023-01-13 16:01:58 +0200

    put some logic in statistics

--- src/components/statistics/Statistics.tsx
+++ src/components/statistics/Statistics.tsx
@@ -1,13 +1,20 @@
-import React from "react";
-import "./statistics.css";
+import React from 'react';
+import './Statistics.css';
 
 interface StatisticsProps {
+  correctWords: number;
+  correctChars: number;
+  incorrectWords: number;
+  seconds: number;
 }
 
-const Statistics: React.FC<StatisticsProps> = () => {
+const Statistics: React.FC<StatisticsProps> = ({ correctWords, correctChars, incorrectWords, seconds }) => {
   return (
     <div className="statistics-container">
-      <h2>statistics here</h2>
+      <h2>statistics</h2>
+      <h3>WPM: {correctWords / seconds}</h3>
+      <h3>CPM: {correctChars / seconds}</h3>
+      <h3>Accuracy: {(incorrectWords / correctWords) * 100}</h3>
     </div>
   );
 };
