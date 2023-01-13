2147c273551576f439d4312e12b747350ac91464
ronnahom96 <ronnahom96@gmail.com>
2023-01-13 19:31:05 +0200

    statistics logic work

--- src/components/statistics/Statistics.tsx
+++ src/components/statistics/Statistics.tsx
@@ -5,16 +5,15 @@ interface StatisticsProps {
   correctWords: number;
   correctChars: number;
   incorrectWords: number;
-  seconds: number;
 }
 
-const Statistics: React.FC<StatisticsProps> = ({ correctWords, correctChars, incorrectWords, seconds }) => {
+const Statistics: React.FC<StatisticsProps> = ({ correctWords, correctChars, incorrectWords }) => {
   return (
     <div className="statistics-container">
       <h2>statistics</h2>
-      <h3>WPM: {correctWords / seconds}</h3>
-      <h3>CPM: {correctChars / seconds}</h3>
-      <h3>Accuracy: {(incorrectWords / correctWords) * 100}</h3>
+      <h3 className="wpm">WPM: {correctWords}</h3>
+      <h3>CPM: {correctChars}</h3>
+      {correctWords > 0 && <h3>Accuracy: {Math.round((correctWords / (incorrectWords + correctWords)) * 100)}%</h3>}
     </div>
   );
 };
